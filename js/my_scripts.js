a12df7b986764645e7b0bdc973aec8bed519f4de
최서연 <20220855@dongduk.ac.kr>
2023-12-21 23:48:00 +0900

    detail

--- js/my_scripts.js
+++ js/my_scripts.js
@@ -4,7 +4,7 @@ function click1() {
     document.getElementById("file").innerHTML = path;
 }
 function click2() {
-    var lab02 = "../lab04/report/index.html";
+    var lab02 = "../lab04/report/indexl2.html";
     var path = '<iframe src="' + lab02 + '" style="width: 100%; height: 100vh; border:none;"></iframe>'
     document.getElementById("file").innerHTML = path;
 }
