5783c4e2ff8331129c59ac1549088987e3f7d153
최서연 <20220855@dongduk.ac.kr>
2023-12-21 23:05:39 +0900

    javascript

--- js/my_scripts.js
+++ js/my_scripts.js
@@ -1,23 +1,20 @@
 function click1() {
     var lab01 = "../lab04/report/index.html";
-    var path = '<iframe src="' + lab01 + '"></iframe>'
+    var path = '<iframe src="' + lab01 + '" style="width: 100%; height: 100vh; border:none;"></iframe>'
     document.getElementById("file").innerHTML = path;
 }
 function click2() {
     var lab02 = "../lab04/report/index.html";
-    var path = '<iframe src="' + lab02 + '"></iframe>'
+    var path = '<iframe src="' + lab02 + '" style="width: 100%; height: 100vh; border:none;"></iframe>'
     document.getElementById("file").innerHTML = path;
 }
 function click3() {
     var lab03 = "../lab04/report/indexl3.html";
-    var path = '<iframe src="' + lab03 + '"></iframe>'
+    var path = '<iframe src="' + lab03 + '" style="width: 100%; height: 100vh; border:none;"></iframe>'
     document.getElementById("file").innerHTML = path;
 }
 function click4() {
     var lab04 = "../lab04/report/indexl4.html";
-    var path = '<iframe src="' + lab04 + '"></iframe>'
+    var path = '<iframe src="' + lab04 + '" style="width: 100%; height: 100vh; border:none;"></iframe>'
     document.getElementById("file").innerHTML = path;
 }
-
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]')
-);
